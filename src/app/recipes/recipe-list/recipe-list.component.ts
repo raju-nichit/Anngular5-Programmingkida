f95850edfd768cb4bfb1c2c9920f56a1f7f5c957
raju-nichit <meetrajnichit@gail.com>
2018-01-24 12:16:35 +0530

    Recipe Model added and navbar added in the header component

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,17 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes = [];
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
