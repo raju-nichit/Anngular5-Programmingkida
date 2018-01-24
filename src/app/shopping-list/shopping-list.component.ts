2d815f8801c90e6481fda9c57546c942c82fb8f9
raju-nichit <meetrajnichit@gail.com>
2018-01-24 12:53:15 +0530

    Ingredient model created in shared folder Shopping list show code added in shopping-list component

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,8 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10),
+  ];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
