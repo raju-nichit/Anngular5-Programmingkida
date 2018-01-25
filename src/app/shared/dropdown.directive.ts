01b03d83bf9567d57b0d23d515ce33709dec4e49
raju-nichit <meetrajnichit@gail.com>
2018-01-25 16:34:16 +0530

    dropdown directive added

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -4,7 +4,6 @@ import {Directive, HostBinding, HostListener} from '@angular/core';
   selector: '[appDropdown]'
 })
 export class DropdownDirective {
-  isOpen = false;
   @HostBinding('class.open') isOpen = false;
 
   constructor() {
